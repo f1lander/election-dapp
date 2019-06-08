833c25269d3e0574dd283fd32bf74543575243df
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2019-06-07 23:51:27 -0600

    first commite smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.8;
+
+contract Election {
+
+    // Store cnadidate
+    //Read candidate
+    string public candidate;
+    // Cosntructor
+    constructor () public{
+        candidate = "Candidate 1";
+    }
+
+}
